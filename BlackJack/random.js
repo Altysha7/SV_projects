70153b36d4df6a3bb2f815fa796198ae5d69cbde
Altysha <k.altysha7@gmail.com>
2023-11-19 16:59:12 +0800

    no message

---
+++ BlackJack/random.js
@@ -0,0 +1,126 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const allBtn = document.querySelectorAll('button')
+const allCardsClass = document.querySelectorAll('.cards')
+const pointsCount = document.querySelectorAll('p')
+
+const playerBtn = allBtn[0]
+const casinoBtn = allBtn[1]
+const startBtn = allBtn[2]
+
+const playerDiv = allCardsClass[0]
+const casinoDiv = allCardsClass[1]
+
+const playerPointsBox = pointsCount[0]
+const casinoPointsBox = pointsCount[1]
+
+
+const allCards = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 11]
+const allCardsImg = ['./cards/2-C.png', './cards/3-C.png', './cards/4-C.png', './cards/5-C.png', './cards/6-C.png',
+ './cards/7-C.png', './cards/8-C.png', './cards/9-C.png', './cards/10-C.png',
+  './cards/J-C.png', './cards/Q-C.png', './cards/K-C.png', './cards/A-C.png' ]
+
+let playerCards = []
+let casinoCards = []
+
+//Random 
+function getRandomNumber() {
+    const allCardsLength = allCards.length
+    const randomNum = Math.floor(Math.random() * allCardsLength) 
+    // console.log(randomNum);
+    // const randomCard = allCards[randomNum]
+    // console.log(randomCard);
+    return randomNum
+}
+
+
+//Player
+playerBtn.addEventListener('click', () => {
+    const randomNumber = getRandomNumber()
+    const valueOfNewCard = allCards[randomNumber]
+    const imgOfNewCard = allCardsImg[randomNumber]
+   playerCards.push(valueOfNewCard)
+   //newCard
+   const newCard = document.createElement('p')
+   playerDiv.append(newCard)
+   //newCard.innerText = valueOfNewCard
+
+   //imgCard
+   const imgCard = document.createElement('img')
+   playerDiv.append(imgCard)
+   imgCard.src= imgOfNewCard
+    //TotalAmount
+   const playerTotalAmount = playerCards.reduce((previousSumm, currentItem) => previousSumm + currentItem, 0)
+   playerPointsBox.innerText = `Player Points: ${playerTotalAmount}`
+   const casinoTotalAmount = casinoCards.reduce((firstValue, secondValue) => firstValue + secondValue, 0)
+    if(playerTotalAmount > 21) { 
+            casinoBtn.disabled = true
+            playerBtn.disabled = true
+            playerPointsBox.innerText = `${playerTotalAmount}points, It's okay, you'll get them next time.`
+            casinoPointsBox.innerText = `Congratulations! You won!` 
+    } 
+    if(casinoTotalAmount < 22 && casinoTotalAmount > playerTotalAmount){
+            playerPointsBox.innerText = `Unfortunately, Player lost.`
+    } else if (casinoTotalAmount === playerTotalAmount) {
+            casinoBtn.disabled = true
+            playerBtn.disabled = true
+            casinoPointsBox.innerText = `No winner this time, it's a tie.`
+            playerPointsBox.innerText = `No winner this time, it's a tie.`
+    }
+    playerPointsBox.innerText = `Player Points: ${playerTotalAmount}`
+})
+
+//Casino
+casinoBtn.addEventListener('click', () => { 
+    console.log(casinoPointsBox)
+      playerBtn.disabled = true
+      const randomNumber = getRandomNumber()
+      const valueOfNewCard = allCards[randomNumber]
+      const imgOfNewCard = allCardsImg[randomNumber]
+      casinoCards.push(valueOfNewCard)
+        //newCard
+     const newCard = document.createElement('p')
+     casinoDiv.append(newCard)
+        //newCard.innerText = valueOfNewCard
+        //imgCard
+     const imgCard = document.createElement('img')
+     casinoDiv.append(imgCard)
+     imgCard.src= imgOfNewCard
+        //TotalAmount
+     const casinoTotalAmount = casinoCards.reduce((firstValue, secondValue) => firstValue + secondValue, 0)
+     const playerTotalAmount = playerCards.reduce((previousSumm, currentItem) => previousSumm + currentItem, 0)
+
+     casinoPointsBox.innerText = `Casino Points: ${casinoTotalAmount}`
+
+    
+        if (casinoTotalAmount > 21) {
+            casinoBtn.disabled = true
+            playerBtn.disabled = true          
+            casinoPointsBox.innerText = `${casinoTotalAmount}points, It's okay, you'll get them next time.`
+            playerPointsBox.innerText = `Congratulations! You won!`
+        }
+         if(casinoTotalAmount < 22 && casinoTotalAmount > playerTotalAmount){
+            casinoBtn.disabled = true
+            playerBtn.disabled = true
+            playerPointsBox.innerText = `Unfortunately, Player lost.`
+        } else if (casinoTotalAmount === playerTotalAmount) {
+            casinoBtn.disabled = true
+            playerBtn.disabled = true
+            casinoPointsBox.innerText = `No winner this time, it's a tie.`
+            playerPointsBox.innerText = `No winner this time, it's a tie.`
+        } 
+           casinoPointsBox.innerText = `Casino Points: ${casinoTotalAmount}`
+})
+
+    //starBtn
+    startBtn.addEventListener('click', () => {
+        casinoBtn.disabled = false
+        playerBtn.disabled = false
+        playerCards = []
+        casinoCards = []
+        playerPointsBox.innerText = `Player Points: 0`
+        casinoPointsBox.innerText = `Casino Points: 0`
+        playerDiv.innerHTML = ''
+        casinoDiv.innerHTML = ''
+    })
+})
---
+++ MoreOrLess/script.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const allBtn = document.querySelectorAll('button')
+    const input = document.querySelector('input')
+    const allDiv = document.querySelectorAll('div')
+    let playerTry = 3
+
+    const startGameBtn = allBtn[0]
+    const appendBtn = allBtn[1]
+
+    const resultDiv = allDiv[0]
+    const hintDiv = allDiv[1]
+
+
+    function getRandomNumber() {
+        const randomNumbers = Math.random() //get random number from 0 to 1
+        const floatRandomNumberGameOption = randomNumbers * 10 // get random number from 0 to 3 because we have 10 val
+        const integerRandomGameOption = Math.floor(floatRandomNumberGameOption)
+        return integerRandomGameOption
+    }
+
+    startGameBtn.addEventListener('click', () => {
+        targetNumber = getRandomNumber()
+        playerTry = 3
+        appendBtn.disabled = false
+        input.value = ''
+    })
+
+    let targetNumber = getRandomNumber()
+    appendBtn.addEventListener('click', () => {
+        // console.log(targetNumber, 'Target Number');
+        // console.log(input.value, 'Input Number');
+        // console.log(playerTry, 'Player Chance');
+        if(targetNumber === +input.value){
+            hintDiv.innerText = 'You\'re the winner!'
+            appendBtn.disabled = true
+
+        } else {
+            playerTry--;
+            if(playerTry < 1){
+                appendBtn.disabled = true
+                hintDiv.innerText = 'Unfortunately, you lost.'
+            } else {
+                if(targetNumber < +input.value) {
+                    hintDiv.innerText = 'Your number is greater than the target.'
+                } 
+                if(targetNumber > +input.value) {
+                    hintDiv.innerText = 'Your number is smaller than the target.'
+                } 
+            }
+        
+        }
+    })
+
+})
